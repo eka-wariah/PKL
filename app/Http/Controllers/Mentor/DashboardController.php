b74318a0bb87507bd5ecfa6a8325b043cd414795
taupik3007 <taupikpathurrohman@gmail.com>
2026-06-06 00:20:01 +0700

    add detail attendance mentor

--- app/Http/Controllers/Mentor/DashboardController.php
+++ app/Http/Controllers/Mentor/DashboardController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Mentor;
 
 use App\Http\Controllers\Controller;
+use App\Models\AcademicYear;
+use App\Models\Attendance;
 use App\Models\MentorAssignments;
+use Carbon\CarbonPeriod;
 use App\Models\News;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
@@ -17,20 +21,50 @@ class DashboardController extends Controller
     public function index()
     {
         $students = MentorAssignments::with('student.attendanceToday')
-    ->where('mas_mentor_id', Auth::user()->mentor->mtr_id)
-    ->get();
-    $guidances = News::where('news_mentor_id', Auth::user()->mentor->mtr_id)->where('news_parent_id',null)->get();
+            ->where('mas_mentor_id', Auth::user()->mentor->mtr_id)
+            ->get();
+        $guidances = News::where('news_mentor_id', Auth::user()->mentor->mtr_id)->where('news_parent_id', null)->get();
 
         // dd($students);
-        return view('mentor.dashboard',compact(['students','guidances']));
+        return view('mentor.dashboard', compact(['students', 'guidances']));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function studentAttendance($id)
     {
-        //
+
+    $student= Student::findOrFail($id); 
+        $academicYear = AcademicYear::where('acy_status', 1)->first();
+
+        $startDate = $academicYear->acy_year . '-06-01';
+        $endDate = Carbon::now()->toDateString();
+        $period = CarbonPeriod::create($startDate, $endDate);
+
+        $attendances = Attendance::where('att_std_id', $id)
+            ->whereBetween('att_date', [$startDate, $endDate])
+            ->orderBy('att_date')
+            ->get()
+            ->keyBy('att_date'); // key by att_date biar bisa di-lookup per tanggal
+
+        $report = [];
+        $no = 1;
+
+        foreach ($period as $date) {
+            $dateStr = $date->format('Y-m-d');
+            $attendance = $attendances->get($dateStr); // sekarang bisa lookup by tanggal
+
+            $report[] = [
+                'no'     => $no++,
+                'date'   => $date->format('d-m-Y'),
+                'status' => $attendance?->att_status ?? 4,
+                'time'   => $attendance?->att_time,
+            ];
+        }
+        // dd($report);
+
+        return view('mentor.attendance.index', compact('report','student'));
     }
 
     /**
